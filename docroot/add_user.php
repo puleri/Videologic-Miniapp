6488f70d6b4e7c4a9e43b51e7f50dd40dc890f7f
mp4 <puleri.mp4@gmail.com>
2021-05-23 18:04:25 -0700

    Added CRUD action

---
+++ docroot/add_user.php
@@ -0,0 +1,11 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+require_once "db_connection.php";
+
+// Here we want to follow similar pattern to demo.php 
+$data =
