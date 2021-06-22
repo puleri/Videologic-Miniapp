da32983d0875a0268cf09f9f83abe907b74e052e
mp4 <puleri.mp4@gmail.com>
2021-06-22 13:52:31 -0700

    Added wireframe for Login modal and Support modal

--- client/src/components/Support/Support.js
+++ client/src/components/Support/Support.js
@@ -26,7 +26,7 @@ class Support extends Component {
     fd.append('strEmail', this.state.strEmail)
     fd.append('strMessage', this.state.strMessage)
     // console.log(Array.from(fd));
-    fetch(`http://localhost:9000/Videologic-Miniapp/docroot/demo.php`, {
+    fetch(`http://localhost:9000/docroot/demo.php`, {
       method: 'POST',
       body: fd,
     }).then(res => res.json())
@@ -41,28 +41,15 @@ class Support extends Component {
   render()
   {
     return (
-      <div className='background'>
-        <div className="header-div">
-            <div>
-              <h1 className="support-header">Support Center</h1>
-              <h4 className="header">Need help? Submit a support request and we'll get back to you ASAP.</h4>
-            </div>
-        </div>
-        <br />
-        <br />
+      <div className='contain'>
+      <div className="support-float">
         <div className="form-div">
           <div className="form">
-          <h1 className="form-header">Get in touch</h1>
           <form>
-            <label>First name</label>
+            <label>Name</label>
             <input type="text" id="firstname" placeholder="your name..."
               value={this.state.strFname}
               onChange={e => this.setState({ strFname: e.target.value })}
-            />
-            <label>Last name</label>
-            <input type="text" id="lastname" placeholder="your last name..."
-              value={this.state.strLname}
-              onChange={e => this.setState({ strLname: e.target.value })}
             />
             <label>Email</label>
             <input type="email" id="email" placeholder="your email..."
@@ -70,7 +57,7 @@ class Support extends Component {
               onChange={e => this.setState({ strEmail: e.target.value })}
             />
             <label>Message</label>
-            <textarea rows="10" type="text" id="message" placeholder="your message..."
+            <textarea rows="5" type="text" id="message" placeholder="your message..."
               value={this.state.strMessage}
               onChange={e => this.setState({ strMessage: e.target.value })}
             />
@@ -78,6 +65,13 @@ class Support extends Component {
           </form>
           <Link to="/ThankYou"><button className="load" onClick={this.getPHP}>Send it along</button></Link>
           </div>
+        </div>
+        </div>
+        <div className='background'>
+        <div>
+          <h5 className="support-header">Support Center</h5>
+          <h6 className="header">Need help? Submit a support request and we'll get back to you ASAP.</h6>
+        </div>
         </div>
       </div>
     )
