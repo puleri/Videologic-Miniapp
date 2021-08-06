5cd940f711478ba2a138c4739822fe96342aaf51
mp4 <puleri.mp4@gmail.com>
2021-08-06 13:01:15 -0700

    Added footer button animation

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 
+// import * as firebase from 'firebase';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+var config = {
+  apiKey: "AIzaSyANfy_Z_kjxIi-U4xQPjq6g4WyQR0o4TkI",
+  authDomain: "kaiserwebapp.firebaseapp.com",
+  projectId: "kaiserwebapp",
+  storageBucket: "kaiserwebapp.appspot.com",
+  messagingSenderId: "1075982375673",
+  appId: "1:1075982375673:web:85497c565b6f2be7fea028",
+  measurementId: "G-LSZXN7TQEJ"
+}
+
+// firebase.initializeApp(config);
 
 ReactDOM.render(
   <HashRouter>
