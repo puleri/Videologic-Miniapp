9d8536e9eab4738983c0e82698bf33d00da83c3e
mp4 <puleri.mp4@gmail.com>
2021-08-11 13:48:55 -0700

    Able to access the user password which will be used to authenticate user

--- docroot/admin_login.php
+++ docroot/admin_login.php
@@ -9,8 +9,8 @@ $link = mysqli_connect($db_host, $db_user, $db_password, $db_db);
 
 $strEmail = $_POST['email'];
 $strPassword = $_POST['password'];
-
-$sql = "SELECT COUNT(1) FROM `user` WHERE username = '${strEmail}'";
+//  First check if user exists
+$sql = "SELECT COUNT(1) FROM `user` WHERE username = 'test@test.com'";
 
 $index = [];
 
@@ -24,7 +24,17 @@ array_push($index, $user_row);
 // Pulls the nested 1:0 telling if user exists
 foreach ($index as $v) {
   foreach ($v as $j)  {
-  echo json_encode('value:' . $j);
+    if ($j === "1") {
+      $sql_password = "SELECT password FROM `user` WHERE username = 'test@test.com'";
+      $passValidation = mysqli_query($link,"SELECT password FROM `user` WHERE username = 'test@test.com'");
+      while($passRow = mysqli_fetch_assoc($passValidation))
+      {
+      $check[] = $passRow;
+      // prints password
+      echo json_encode($check);
+      }
+      echo json_encode($j);
+    }
   }
 }
 // echo json_encode($index);
