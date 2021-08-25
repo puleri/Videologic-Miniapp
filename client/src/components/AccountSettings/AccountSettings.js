8fdcf970d8c82215e502c0864effce508b556ac1
mp4 <puleri.mp4@gmail.com>
2021-08-24 20:59:20 -0700

    Added state to tabs so active tab will have an indicator letting user know it has been clicked

--- client/src/components/AccountSettings/AccountSettings.js
+++ client/src/components/AccountSettings/AccountSettings.js
@@ -4,28 +4,66 @@ import "./AccountSettings.css";
 
 import { withRouter, Link } from 'react-router-dom';
 
+
 class AccountSettings extends Component {
   constructor(props){
     super(props)
     this.state = {
-
+      active: 'Profile'
     }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e) {
+    console.log(e.target.innerHTML)
+    this.setState({
+      active: e.target.innerHTML
+    })
   }
   render(){
+    let profileStyle =  {  };
+    let credentialsStyle = { };
+    let permissionsStyle = { };
+    let deleteAcctStyle = { };
+
+    if (this.state.active === 'Profile') {
+      profileStyle =  { color: "black",   borderRight: "5px solid rgb(194,38,15)"};
+      credentialsStyle = { };
+      permissionsStyle = { };
+      deleteAcctStyle = { };
+    }
+    if (this.state.active === "Credentials") {
+      profileStyle =  {  };
+      credentialsStyle = { color: "black", borderRight: "5px solid rgb(194,38,15)" };
+      permissionsStyle = { };
+      deleteAcctStyle = { };
+    }
+    if (this.state.active === "Permissions") {
+      profileStyle =  {  };
+      credentialsStyle = { };
+      permissionsStyle = { color: "black", borderRight: "5px solid rgb(194,38,15)" };
+      deleteAcctStyle = { };
+    }
+    if (this.state.active === "Delete Account") {
+      profileStyle =  {  };
+      credentialsStyle = { };
+      permissionsStyle = { };
+      deleteAcctStyle = { color: "black", borderRight: "5px solid rgb(194,38,15)" };
+    }
+
     return (
       <>
         <div className="settings-container">
           <div className="tab-container">
-            <Link className="tab profile-tab">
+            <Link style={ profileStyle } onClick={this.handleClick} className="tab profile-tab">
               Profile
             </Link>
-            <Link className="tab credentials-tab">
+            <Link style={ credentialsStyle } onClick={this.handleClick} className="tab credentials-tab">
               Credentials
             </Link>
-            <Link className="tab permissions-tab">
-              Permisisons
+            <Link style={ permissionsStyle } onClick={this.handleClick} className="tab permissions-tab">
+              Permissions
             </Link>
-            <Link className="tab deleteAcct-tab">
+            <Link style={ deleteAcctStyle } onClick={this.handleClick} className="tab deleteAcct-tab">
               Delete Account
             </Link>
             <div className="tab-highlight">
