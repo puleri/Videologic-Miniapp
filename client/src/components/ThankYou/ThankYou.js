36995c2d45e586823d045c53425ff2553c38facc
mp4 <puleri.mp4@gmail.com>
2021-09-09 11:55:17 -0700

    Thank you page style update

--- client/src/components/ThankYou/ThankYou.js
+++ client/src/components/ThankYou/ThankYou.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router-dom';
+import vv from './icon.png'
+import './ThankYou.css';
 
 
 class ThankYou extends Component {
   render() {
     return (
       <div className="thank-you-div">
+        <img id="ty-icon" src={vv} alt="VV Icon" />
         <div className="thank-you">
           <p>Thank you. We have recieved your request and should be back to you in the next 48 hours.</p>
           <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
