5d2ca55e38ccf015711c212ed50094e2bb7e92b8
mp4 <puleri.mp4@gmail.com>
2021-09-16 16:34:06 -0700

    useState Hook was not acting as expected-- changed the functional testing component to a class component and am fetching the test cases with it

--- client/src/components/Testing/Testing.js
+++ client/src/components/Testing/Testing.js
@@ -1,57 +1,69 @@
-import React, { useState, useEffect } from 'react'
+import React, { Component } from 'react'
 import './Testing.css';
 
-export default function Testing() {
-  const [added, setAdded] = useState(false)
-  const [tableContents, setTableContents] = useState([])
+export default class Testing extends Component {
+  constructor() {
+    super()
 
+    this.state = {
+      testCases: []
+    }
+  }
 // On load
-  useEffect(() => {
-      fetch(`http://localhost:9000/docroot/test_case_index.php`, {
-        method: 'GET',
-      })
-      .then(res => res.json())
-      .then(response => {
-        // set state by spreading response
-        setTableContents(tableContents => [...tableContents, response])
-      })
-      .then(console.log('table contents are', tableContents))
-      .catch(err=> {
-        console.log(err)
-      });
+indexTests() {
+  // console.log("props is ", this.props)
+  fetch(`http://localhost:9000/docroot/test_case_index.php`, {
+    method: 'GET',
+  })
+  .then(res => res.json())
+  .then(response => {
+    this.setState({ testCases: response })
+    console.log('state is', this.state.testCases)
+  })
+  // .then(this.setState(prevState => ({
+  //   users: {
+  //     ...prevState.users,
+  //
+  //   }
+  // })))
+  .catch(err=> {
+    console.log(err)
+  });
+}
 
-  }, [])
+componentDidMount() {
+  this.indexTests();
+}
 
+// tableContentsHTML() {this.state.testCases.map((el, i) => (
+//   <td key={i}>
+//     {el.id ? 'yes' : 'no'}
+//   </td>
+// ))
 
-const tableContentsHTML = tableContents.map((el, i) => (
-  <td key={i}>
-    {el.id ? 'yes' : 'no'}
-  </td>
-))
-
-
+  render () {
   return (
-    <>
-      <h2 style={{margin: "20px 50px",textAlign:'left'}}>Test Case Planning and Execution</h2>
+      <>
+        <h2 style={{margin: "20px 50px",textAlign:'left'}}>Test Case Planning and Execution</h2>
 
-      <div className="test-case-create">
-        <input placeholder="test description..."/>
-        <input placeholder="date..."/>
-        <input placeholder="expected results..."/>
-        <input placeholder="actual results..."/>
-        <input placeholder="pass/fail..."/>
-        <input placeholder="additional notes..."/>
-      </div>
+        <div className="test-case-create">
+          <input placeholder="test description..."/>
+          <input placeholder="date..."/>
+          <input placeholder="expected results..."/>
+          <input placeholder="actual results..."/>
+          <input placeholder="pass/fail..."/>
+          <input placeholder="additional notes..."/>
+        </div>
 
-      <div className="test-case-notes">
+        <div className="test-case-notes">
 
-      </div>
+        </div>
 
-      <div className="test-case-table">
-        <table>
-          {tableContentsHTML}
-        </table>
-      </div>
-    </>
-  )
+        <div className="test-case-table">
+          <table>
+          </table>
+        </div>
+      </>
+    )
+  }
 }
