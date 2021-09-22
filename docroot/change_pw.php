709c48087a8e266ca4729315a82b42423cd3406f
mp4 <puleri.mp4@gmail.com>
2021-09-22 10:57:58 -0700

    successfully queries the previous password in the credentials form. next is adding the conditional and combining with functional update query

--- docroot/change_pw.php
+++ docroot/change_pw.php
@@ -12,26 +12,40 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-// Check if old password matches credentials
-if($strOldPW === $sqlOldPW) {
-  
+$validation = "SELECT `password` FROM `user` WHERE `token` = '${strToken}'";
+
+$strOldPass = [];
+
+$validationResult = $link->query($validation);
+
+while($row = $validationResult->fetch_assoc()) {
+
+$strOldPass_Row = ['password'=> $row['password']];
+
+array_push($strOldPass, $strOldPass_Row);
 }
+
+echo json_encode($strOldPass);
+// Check if old password matches credentials
+// if($strOldPW === $strOldPass) {
+//
+// }
 
 // Update password SQL
-$sql = "UPDATE `user` SET `password` = '${strNewPW}' WHERE `token` LIKE '${strToken}'";
-
-$user = [];
-
-$result = $link->query($sql);
-
-while($row = $result->fetch_assoc()) {
-
-$user_row = ['strUsername'=> $row['username']] ;
-
-array_push($user, $user_row);
-}
-
-echo json_encode($user);
+// $sql = "UPDATE `user` SET `password` = '${strNewPW}' WHERE `token` LIKE '${strToken}'";
+//
+// $user = [];
+//
+// $result = $link->query($sql);
+//
+// while($row = $result->fetch_assoc()) {
+//
+// $user_row = ['strUsername'=> $row['username']] ;
+//
+// array_push($user, $user_row);
+// }
+//
+// echo json_encode($user);
 
 
 $link->close();
