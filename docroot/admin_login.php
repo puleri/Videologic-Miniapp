990bece07520fd9a3f5736ce017e784c6fc8721d
mp4 <puleri.mp4@gmail.com>
2021-09-24 11:22:46 -0700

    OO Login functional. More Conversion to OOP on PHP side
    Due to the need for another login at the delete
    account page and the simplicity of having these functions
    more accessible, I have converted more procedural code into
    Object Oriented.

--- docroot/admin_login.php
+++ docroot/admin_login.php
@@ -1,58 +1,68 @@
 <?php
+require_once './classes/user.php';
 
-require_once './db_connection.php';
-require_once './admin_creds.php';
-
-require_once '../vendor/autoload.php';
-
-$link = mysqli_connect($db_host, $db_user, $db_password, $db_db);
-
+// require_once './db_connection.php';
+// require_once './admin_creds.php';
+// //
+// require_once '../vendor/autoload.php';
+//
+// $link = mysqli_connect($db_host, $db_user, $db_password, $db_db);
+//
 $strEmail = $_POST['email'];
 $strPassword = $_POST['password'];
-//  First check if user exists
-$sql = "SELECT COUNT(1) FROM `user` WHERE username = '${strEmail}'";
 
-$index = [];
+// $strEmail = '1';
+// $strPassword = '1';
 
-$result = $link->query($sql);
 
-while($row = $result->fetch_assoc())
-{
-$user_row = ['COUNT(1)'=> $row['COUNT(1)']] ;
-array_push($index, $user_row);
-}
-// Pulls the nested 1:0 telling if user exists
-foreach ($index as $v) {
-  foreach ($v as $j)  {
-    if ($j === "1") {
-      // $sql_password = "SELECT password FROM `user` WHERE username = '${strEmail}'";
-      $passValidation = mysqli_query($link,"SELECT password FROM `user` WHERE username = '${strEmail}'");
-      while($passRow = mysqli_fetch_assoc($passValidation))
-      {
-      $check[] = $passRow;
-      // prints password
-      $strValidPassword = $check[0]['password'];
-      // cleaning the data
-      substr_replace($strValidPassword ,"", -1);
-      // echo json_encode($strValidPassword);
-      if ($strValidPassword == $strPassword)
-        {
-          $bytes = random_bytes(20);
-          $strBytes = strval(bin2hex($bytes));
-          // set the login token SQL
-          $tokenSQL = "UPDATE `user` SET `token` = '${strBytes}' WHERE `user`.`username` = '${strEmail}'";
-          mysqli_query($link, $tokenSQL);
-          echo json_encode("Logging in..." . $strBytes);
-        }
-      }
-      // Returning 1 or 0 if user exists
-      // echo json_encode($j);
-    }
-  }
-}
-// echo json_encode($index);
+$objUser = new User('', $strEmail, $strPassword);
 
-mysqli_close($link);
+$objUser->login_user();
+
+// //  First check if user exists
+// $sql = "SELECT COUNT(1) FROM `user` WHERE username = '${strEmail}'";
+//
+// $index = [];
+//
+// $result = $link->query($sql);
+//
+// while($row = $result->fetch_assoc())
+// {
+// $user_row = ['COUNT(1)'=> $row['COUNT(1)']] ;
+// array_push($index, $user_row);
+// }
+// // Pulls the nested 1:0 telling if user exists
+// foreach ($index as $v) {
+//   foreach ($v as $j)  {
+//     if ($j === "1") {
+//       // $sql_password = "SELECT password FROM `user` WHERE username = '${strEmail}'";
+//       $passValidation = mysqli_query($link,"SELECT password FROM `user` WHERE username = '${strEmail}'");
+//       while($passRow = mysqli_fetch_assoc($passValidation))
+//       {
+//       $check[] = $passRow;
+//       // prints password
+//       $strValidPassword = $check[0]['password'];
+//       // cleaning the data
+//       substr_replace($strValidPassword ,"", -1);
+//       // echo json_encode($strValidPassword);
+//       if ($strValidPassword == $strPassword)
+//         {
+//           $bytes = random_bytes(20);
+//           $strBytes = strval(bin2hex($bytes));
+//           // set the login token SQL
+//           $tokenSQL = "UPDATE `user` SET `token` = '${strBytes}' WHERE `user`.`username` = '${strEmail}'";
+//           mysqli_query($link, $tokenSQL);
+//           echo json_encode("Logging in..." . $strBytes);
+//         }
+//       }
+//       // Returning 1 or 0 if user exists
+//       // echo json_encode($j);
+//     }
+//   }
+// }
+// // echo json_encode($index);
+//
+// mysqli_close($link);
 
 // $token = hash('sha256', uniqid());
 // echo "hello, $token";
--- docroot/classes/user.php
+++ docroot/classes/user.php
@@ -1,15 +1,76 @@
 <?php
 
+
+
 class User {
   public $test = "test";
 
-  function __construct($strId)
+  function __construct($strId, $strEmail, $strPassword)
   {
     $this->strId = $strId;
+    $this->strEmail = $strEmail;
+    $this->strPassword = $strPassword;
+
   }
 
   public function test() {
     print_r($this->strId);
+
+  }
+
+  public function login_user() {
+    require_once './db_connection.php';
+    require_once './admin_creds.php';
+    //
+    require_once '../vendor/autoload.php';
+    $link = mysqli_connect($db_host, $db_user, $db_password, $db_db);
+
+    // $strEmail = $_POST['email'];
+    // $strPassword = $_POST['password'];
+    //  First check if user exists
+    $sql = "SELECT COUNT(1) FROM `user` WHERE username = '$this->strEmail'";
+
+    $index = [];
+
+    $result = $link->query($sql);
+
+    while($row = $result->fetch_assoc())
+    {
+      $user_row = ['COUNT(1)'=> $row['COUNT(1)']] ;
+      array_push($index, $user_row);
+    }
+    // Pulls the nested 1:0 telling if user exists
+    foreach ($index as $v) {
+      foreach ($v as $j)  {
+        if ($j === "1") {
+          // $sql_password = "SELECT password FROM `user` WHERE username = '${strEmail}'";
+          $passValidation = mysqli_query($link,"SELECT password FROM `user` WHERE username = '$this->strEmail'");
+          while($passRow = mysqli_fetch_assoc($passValidation))
+          {
+            $check[] = $passRow;
+            // prints password
+            $strValidPassword = $check[0]['password'];
+            // cleaning the data
+            substr_replace($strValidPassword ,"", -1);
+            // echo json_encode($strValidPassword);
+            if ($strValidPassword == $this->strPassword)
+            {
+              $bytes = random_bytes(20);
+              $strBytes = strval(bin2hex($bytes));
+              // set the login token SQL
+              $tokenSQL = "UPDATE `user` SET `token` = '${strBytes}' WHERE `user`.`username` = '${strEmail}'";
+              mysqli_query($link, $tokenSQL);
+              echo json_encode("Logging in..." . $strBytes);
+            }
+          }
+          // Returning 1 or 0 if user exists
+          // echo json_encode($j);
+        }
+      }
+    }
+    // echo json_encode($index);
+
+    mysqli_close($link);
 
   }
 
